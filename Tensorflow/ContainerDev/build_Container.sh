b3f7b244136dc126c3390b7103ac56115f0b4724
William Hanau <Willhanau@users.noreply.github.com>
2018-05-21 19:05:35 -0700

    Update build_Container.sh

--- Tensorflow/ContainerDev/build_Container.sh
+++ Tensorflow/ContainerDev/build_Container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker_repo="willhanau/tensorflow"
-docker_tag="cuda9.0-cudnn7.0.5-tf1.8"
-docker_file="Dockerfile_Cuda9.0_Tf1.8_build"
+docker_tag="cuda9.0-cudnn7.0.5-tf1.8-horovod"
+docker_file="Dockerfile_Cuda9.0_Tf1.8_Horovod_build"
 
 docker build -t $docker_repo:$docker_tag -f ./$docker_file .
 docker push $docker_repo:$docker_tag
